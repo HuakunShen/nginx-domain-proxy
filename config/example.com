321a81cdf9ecc7414100e8332930f27c8a8e6912
Huakun Shen <huakun.shen@huakunshen.com>
2021-10-18 20:49:43 -0400

    Fixed some bugs
    wrong comment, entrypoint bug, docker-compose.yml bug

--- config/example.com
+++ config/example.com
@@ -5,7 +5,7 @@ server {
     server_name example.com;
 
     location / {
-        proxy_pass http://192.168.1.102:9000;       // TODO: change the ip
+        proxy_pass http://192.168.1.102:9000;
         proxy_set_header Host example.com;
     }
 }
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 for config_file in $(ls /etc/nginx/sites-available | grep -v default);
 do
-    ln -s /etc/nginx/sites-available/$config_file /etc/nginx/sites-enabled;
+	if [ ! -f /etc/nginx/sites-enabled/$config_file ]; then
+		ln -s /etc/nginx/sites-available/$config_file /etc/nginx/sites-enabled/$config_file;
+	fi
 done
 nginx -g "daemon off;";
